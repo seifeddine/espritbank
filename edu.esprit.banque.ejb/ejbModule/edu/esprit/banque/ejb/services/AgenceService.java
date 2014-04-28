2ad3f5c0d671083adf2be89a02f97c6f64ca5bf7
FaresBenAmmar <fares.benammar@esprit.tn>
2014-04-28 01:17:43 +0100

    Commit Agence Service

---
+++ edu.esprit.banque.ejb/ejbModule/edu/esprit/banque/ejb/services/AgenceService.java
@@ -0,0 +1,12 @@
+package edu.esprit.banque.ejb.services;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+import edu.esprit.banque.domain.Agence;
+public class AgenceService  {
+
+	
+	
+}
