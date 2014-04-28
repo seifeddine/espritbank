0cc49822c546ed62d4a0108e6685faed5d4e917c
FaresBenAmmar <fares.benammar@esprit.tn>
2014-04-28 01:41:02 +0100

    AgenceServiceLocal + AgenceServiceRemote

--- edu.esprit.banque.ejb/ejbModule/edu/esprit/banque/ejb/services/AgenceService.java
+++ edu.esprit.banque.ejb/ejbModule/edu/esprit/banque/ejb/services/AgenceService.java
@@ -1,11 +1,38 @@
 package edu.esprit.banque.ejb.services;
+import java.util.List;
+
 import javax.ejb.Stateless;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
 
 import edu.esprit.banque.domain.Agence;
-public class AgenceService  {
+
+
+
+@Stateless
+public class AgenceService implements AgenceServiceLocal, AgenceServiceRemote {
+
+	@PersistenceContext(name="banquePU")
+	private EntityManager entityManager;
+	
+	@Override
+	public List<Agence> findAllAgence() {
+		return entityManager.createQuery("select c from Personne c where c.isadmin=0",Agence.class).getResultList();
+	}
+
+	@Override
+	public void addAgence(Agence agence) {
+		entityManager.persist(agence);
+		
+	}
+
+	@Override
+	public void updateAgence(Agence agence) {
+		entityManager.merge(agence);
+		
+	}
 
 	
 	
